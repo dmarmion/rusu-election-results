77f38f42f6ffe97921165227f890b8d9c2f906b3
dmarmion <dmarmion08@gmail.com>
2023-07-18 00:29:09 +1000

    Add 2021 election data

--- src/utils/teams.ts
+++ src/utils/teams.ts
@@ -1,12 +1,14 @@
 // Identifiers for each team
 export const TEAM_ID_TOGETHER = "group_tog";
 export const TEAM_ID_SOCIAL_JUSTICE = "group_soj";
+export const TEAM_ID_STUDENT_VOICE = "group_stv";
 export const TEAM_ID_MOMENTUM = "group_mom";
 
 // Stores the tailwind background colour associated with each team
 const TEAM_COLOURS = new Map([
   [TEAM_ID_TOGETHER, "bg-sky-500"],
   [TEAM_ID_SOCIAL_JUSTICE, "bg-neutral-800"],
+  [TEAM_ID_STUDENT_VOICE, "bg-red-500"],
   [TEAM_ID_MOMENTUM, "bg-fuchsia-400"],
 ]);
 
