d2281401a8d3594747af7a4fe069e641a0e9952b
dmarmion <dmarmion08@gmail.com>
2023-09-27 00:56:39 +1000

    Align vote share bars to bottom of boxes

--- src/components/CouncilMember.tsx
+++ src/components/CouncilMember.tsx
@@ -40,40 +40,45 @@ export default function CouncilMember({ positionID, candidates }: CouncilMemberP
   return (
     <div className="flex flex-col">
       <h3>{labelForPosition(positionID)}</h3>
-      <div className="grow rounded-lg bg-neutral-200 p-2">
+      <div className="flex grow flex-col rounded-lg bg-neutral-200 p-2">
         {/* Candidate square(s) */}
-        {Array(winner.names.length)
-          .fill(null)
-          .map(() => (
-            <CandidateSquare teamID={winner.team} />
-          ))}
-
-        {/* Results table */}
-        <table className="mb-4 w-full">
-          <thead>
-            <tr className="border-b-2 border-gray-700 text-left">
-              <th>Candidate</th>
-              <th className="px-2">Team</th>
-              <th className="px-2">Votes</th>
-              <th className="px-2">%</th>
-            </tr>
-          </thead>
-          <tbody>
-            {candidates.map(({ names, team, votes }, idx) => (
-              <tr
-                className="border-b border-gray-400 data-[winner=true]:font-bold"
-                data-winner={winnerIndex === idx}
-              >
-                <td className="py-2">{names.map((name) => <p>{name}</p>) ?? UNKNOWN_CANDIDATE}</td>
-                <td className="p-2">
-                  <TeamNameWithDot teamID={team} />
-                </td>
-                <td className="p-2">{votes ?? NOT_APPLICABLE}</td>
-                <td className="p-2">{votePercentMessage(votes, votesCast)}</td>
-              </tr>
+        <div className="inline">
+          {Array(winner.names.length)
+            .fill(null)
+            .map(() => (
+              <CandidateSquare teamID={winner.team} />
             ))}
-          </tbody>
-        </table>
+        </div>
+        {/* Results table */}
+        <div className="grow">
+          <table className="mb-4 w-full">
+            <thead>
+              <tr className="border-b-2 border-gray-700 text-left">
+                <th>Candidate</th>
+                <th className="px-2">Team</th>
+                <th className="px-2">Votes</th>
+                <th className="px-2">%</th>
+              </tr>
+            </thead>
+            <tbody>
+              {candidates.map(({ names, team, votes }, idx) => (
+                <tr
+                  className="border-b border-gray-400 data-[winner=true]:font-bold"
+                  data-winner={winnerIndex === idx}
+                >
+                  <td className="py-2">
+                    {names.map((name) => <p>{name}</p>) ?? UNKNOWN_CANDIDATE}
+                  </td>
+                  <td className="p-2">
+                    <TeamNameWithDot teamID={team} />
+                  </td>
+                  <td className="p-2">{votes ?? NOT_APPLICABLE}</td>
+                  <td className="p-2">{votePercentMessage(votes, votesCast)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <VoteShareBar
           teamVotes={candidates.map(({ team, votes }) => ({
             teamID: team,
